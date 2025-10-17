5c129386135c5d830e865fd65b0530e3f23a3ea9
Istiak <new.life3618@gmail.com>
2025-10-17 23:35:57 +0600

    format_fix: added proper comment with explanation

--- temp_dir_cleanup/temp_dir_cleanup.py
+++ temp_dir_cleanup/temp_dir_cleanup.py
@@ -7,17 +7,17 @@ import datetime
 temp_dir="/tmp" # temp file dir
 days_old=7   # Files older than this will be deleted
 
-now = datetime.datetime.now()
-delete_file = now - datetime.timedelta(days=days_old)
+now = datetime.datetime.now()   # store the value of current date and time
+delete_file = now - datetime.timedelta(days=days_old) # stored the value time between the files needs to be deleted
 
-deleted_files = 0
-deleted_dirs = 0
+deleted_files = 0   # store how many files deleted
+deleted_dirs = 0    # store how many dir deleted
 
 for root, dirs, files in os.walk(temp_dir, topdown=False): # delete old files
     for name in files:
-        file_path = os.path.join(root, name)
+        file_path = os.path.join(root, name)    # make the full path with the current or root dir 
         try:
-            mtime = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+            mtime = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))    # get the file age
             if mtime < days_old:
                 os.remove(file_path)    # delet empty old file
                 deleted_files += 1
@@ -25,11 +25,11 @@ for root, dirs, files in os.walk(temp_dir, topdown=False): # delete old files
             print(f"Error deleting file {file_path}: {e}")
     
     for name in dirs:
-        dir_path = os.path.join(root, name)
+        dir_path = os.path.join(root, name)     # make a full dir path 
         try:
-            mtime = datetime.datetime.fromtimestamp(os.path.getmtime(dir_path))
-            if mtime < days_old and not os.listdir(dir_path):
-                shutil.rmtree(dir_path)
+            mtime = datetime.datetime.fromtimestamp(os.path.getmtime(dir_path))     # get dir age
+            if mtime < days_old and not os.listdir(dir_path):   # check the dir age is less than 7 and the dir is not empty
+                shutil.rmtree(dir_path)     # remove dir and its content recursively
                 deleted_dirs += 1
         except Exception as e:
             print(f"Error deleting directory {dir_path}: {e}")
